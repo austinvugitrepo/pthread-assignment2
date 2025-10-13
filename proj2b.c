527b983de28d9c3e68b87f000d3a6376375381c6
Austin <freeluck.v.i.p999@gmail.com>
2025-10-13 13:15:07 -0500

    started proj2b.c, will update soon

---
+++ proj2b.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//pointer is array
+int *Y; //resultant vector
+int **A; //matrix   
+int *X; //vector
+	// Y = AX where A is m*n, X is n (n*1), Y is m (m*1) 
+int m; //rows
+int n; //columns
+
+int main() {
+
+printf("Enter the amount of rows: ");
+scanf("%d", &m);
+printf("Enter the amount of columns: ");
+scanf("%d", &n);
+
+
+
+
+
+
+
+
+
+
+
+
+}
