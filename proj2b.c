2560941a8c62a368429979ba736693373367e355
Austin <freeluck.v.i.p999@gmail.com>
2025-10-13 15:30:23 -0500

    allocated memory, filled matrix and vector and printed, probably more stuff i did, go check

--- proj2b.c
+++ proj2b.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 //pointer is array
 int *Y; //resultant vector
@@ -16,11 +17,56 @@ scanf("%d", &m);
 printf("Enter the amount of columns: ");
 scanf("%d", &n);
 
+Y = malloc(m * sizeof(int)); //allocating memory for result vector (Y)
 
+A = malloc(m * sizeof(int*)); //allocating memory for matrix (A)
+for( int i = 0; i < m; i++){
 
+A[i] = malloc(n * sizeof(int)); //allocating memory for columns of matrix
 
+}
 
+X = malloc(n * sizeof(int)); //allocating memory for vector (X)
 
+srand(time(NULL)); // seeding with epoch time
+
+for( int i = 0; i < m; i++){
+
+for( int j = 0; j < n; j++){
+A[i][j] = rand() % 100;      // filling matrix (A) with random num
+}
+
+}
+
+for( int i = 0; i < n; i++){
+X[i] = rand() % 100;           // filling vector (x) with random num
+}
+
+printf("Matrix:\n");
+printf("-----------------------\n");
+for( int i = 0; i < m; i++){           //printing the matrix
+
+for( int j = 0; j < n; j++){
+printf("%3d ", A[i][j]);
+}
+printf("\n\n"); //space to break up output
+}
+
+printf("Vector:\n");
+printf("-----------------------\n");
+for( int i = 0; i < n; i++){
+printf("%3d ", X[i]);             //printing the vector
+}
+printf("\n\n");
+
+for( int i = 0; i < m; i++){
+
+free(A[i]);  //releasing column from a after completion
+
+}
+free(A);
+free(Y);  // releasing memory after completion
+free(X);
 
 
 
